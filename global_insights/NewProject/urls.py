679ce387b1fc9340f8263bb98809098af518af00
development0261 <development0261@gmail.com>
2023-04-12 18:21:32 +0530

    Changes 12-4-2023

--- global_insights/NewProject/urls.py
+++ global_insights/NewProject/urls.py
@@ -1,9 +1,10 @@
 from . import views
 from django.urls import path
-urlpatterns=[
-    path('',views.index,name='global_insights'),
-    path('aboutus',views.about_us,name='about_us'),
-    path('our_services',views.service_1,name='services'),
-    path('services',views.service_2,name='services2'),
-    path('contact_us',views.about_us,name='contact_us'),
+
+urlpatterns = [
+    path("", views.index, name="global_insights"),
+    path("aboutus/", views.about_us, name="about_us"),
+    path("our_services/", views.service_1, name="services"),
+    # path("schedule/", views.schedule_view, name="meeting_schedule"),
+    path("contact_us/", views.contact_, name="contact_us"),
 ]
--- global_insights/NewProject/views.py
+++ global_insights/NewProject/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render
 
+# import calendly
+# from calendlyapi import Client
+
+
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request, "index.html")
+
 
 def about_us(request):
-    return render(request,'about-us.html')
+    return render(request, "about-us.html")
 
-def contact_us(request):
-    return render(request,'contact_us.html')
 
-def service_2(request):
-    return render(request,'service2.html')
+def contact_(request):
+    return render(request, "contact_us.html")
+
 
 def service_1(request):
-    return render(request,'services.html')
+    return render(request, "services.html")
+
+
+def calendly_(request):
+    return render(request, "calendly.html")
+
+
+# def schedule_view(request):
+#     client = Client(api_key="YOUR_API_KEY")
+#     event_type = client.get_event_type("YOUR_EVENT_TYPE_ID")
+#     event_link = event_type["url"]
+#     return render(request, "schedule.html", {"event_link": event_link})
