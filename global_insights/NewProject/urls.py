ee1104120dd6a5e1abd4ed213f40a9b99c1e61d6
Ubuntu <ubuntu@ip-172-31-2-196.ap-southeast-2.compute.internal>
2023-05-17 05:03:22 +0000

    Changes 17-5-2023

--- global_insights/NewProject/urls.py
+++ global_insights/NewProject/urls.py
@@ -1,5 +1,8 @@
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index, name="global_insights"),
@@ -7,3 +10,4 @@ urlpatterns = [
     path("our_services/", views.service_1, name="services"),
     path("contact_us/", views.contact_, name="contact_us"),
 ]
+urlpatterns += staticfiles_urlpatterns()
--- global_insights/NewProject/views.py
+++ global_insights/NewProject/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
 # import calendly
 # from calendlyapi import Client
 
 
 # Create your views here.
+@csrf_exempt
 def index(request):
     return render(request, "index.html")
 
-
+@csrf_exempt
 def about_us(request):
     return render(request, "about-us.html")
 
-
+@csrf_exempt
 def contact_(request):
     return render(request, "contact_us.html")
 
-
+@csrf_exempt
 def service_1(request):
     return render(request, "services.html")
 
-
+@csrf_exempt
 def calendly_(request):
     return render(request, "calendly.html")
 
